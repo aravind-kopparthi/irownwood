ed44f0aa842a0cac194f96b3b77a9ecddd4624b9
mkopparthi <36413278+mkopparthi@users.noreply.github.com>
2018-02-12 10:51:18 -0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,12 +5,12 @@ define('HOST', 'databases-auth.000webhost.com');
 define('USERNAME', 'id4502750_admin');
 define('PASSWORD', 'admin123');
 define('DATABASE_NAME', 'id4502750_ironwooddb');
-echo "Connecting...."
+echo "Connecting....";
 //Connect with the database
 $db = new mysqli(HOST, USERNAME, PASSWORD, DATABASE_NAME);
 if($db->connect_errno):
     die('Connect error:'.$db->connect_error);
 else
-  echo "connection successful"
+  echo "connection successful";
 endif;
 ?>
