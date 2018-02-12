90841f0e09ae1d0b873844b4d591273999fcc5c8
mkopparthi <36413278+mkopparthi@users.noreply.github.com>
2018-02-12 11:08:06 -0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -8,9 +8,8 @@ define('DATABASE_NAME', 'id4502750_ironwooddb');
 echo "Connecting....";
 //Connect with the database
 $db = new mysqli(HOST, USERNAME, PASSWORD, DATABASE_NAME);
-if($db->connect_errno):
+if($db->connect_errno){
     die('Connect error:'.$db->connect_error);
-else
-  echo "connection successful";
-endif;
+}
+echo "Connected successfully";
 ?>
